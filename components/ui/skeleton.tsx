39a601f6f6998841a5f36248ffc520a34d84c972
Creativecoder11 <creativecoderpro@gmaol.com>
2026-05-17 13:01:27 +0600

    Website User Role Change

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import { cn } from "@/lib/utils";
 
-export function Skeleton({ className }: { className?: string }) {
+export function Skeleton({ className, style }: { className?: string; style?: React.CSSProperties }) {
   return (
     <div
+      style={style}
       className={cn(
         "relative overflow-hidden rounded-md bg-gray-100 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.4s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent",
         className,
